b70f67501f08364b2ecaff44ead50c77664d5f99
GarrettLake <garrett.e.lake@gmail.com>
2017-02-17 10:17:00 -0600

    test

---
+++ test2.ps1
@@ -0,0 +1,14 @@
+Get-WmiObject -Namespace 'root\webadministration' -Class ApplicationPool -ComputerName computernamehere ` -Authentication 6 -Filter "Name='serverA'" | Invoke-WmiMethod -Name Stop
+
+$appPoolName = "serverA"
+$appPoolStatus = Invoke-Command -ComputerName computernamehere { param($apn) Import-Module WebAdministration; (Get-WebAppPoolState $apn).Value;} -Args $appPoolName
+add-content -path C:\test\logs\test.txt -value "serverA app pool is $appPoolStatus"
+
+echo $appPoolStatus
+
+Get-WmiObject -Namespace 'root\webadministration' -Class ApplicationPool -ComputerName computernamehere ` -Authentication 6 -Filter "Name='serverA'" | Invoke-WmiMethod -Name Start
+
+$appPoolStatus = Invoke-Command -ComputerName computernamehere { param($apn) Import-Module WebAdministration; (Get-WebAppPoolState $apn).Value;} -Args $appPoolName
+add-content -path C:\test\logs\test.txt -value "serverA app pool is $appPoolStatus"
+
+echo $appPoolStatus
