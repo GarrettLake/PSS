00fbd3c8bd475e17f5b975f42dcbb4bfd1672f8d
GarrettLake <garrett.e.lake@gmail.com>
2017-02-27 09:58:21 -0600

    Create testjenkinsMHScript.ps1

---
+++ testjenkinsMHScript.ps1
@@ -0,0 +1,18 @@
+#Script pulled from https://hodgkins.io/ uploaded to test with jenkins
+$service = "$env:Service"
+$ScriptBlockContent = { 
+    param($Service = $env:Service)
+    Restart-Service -Name "$Service"}
+
+# Ensure the build fails if there is a problem.
+# The build will fail if there are any errors on the remote machine too!
+$ErrorActionPreference = 'Stop'
+
+# Create a PSCredential Object using the "User" and "Password" parameters that you passed to the job
+$SecurePassword = $env:Password | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $env:User, $SecurePassword
+
+
+# Invoke a command on the remote machine.
+# It depends on the type of job you are executing on the remote machine as to if you want to use "-ErrorAction Stop" on your Invoke-Command.
+Invoke-Command -ComputerName $env:Computer -Credential $cred -ScriptBlock $ScriptBlockContent -ArgumentList $service
